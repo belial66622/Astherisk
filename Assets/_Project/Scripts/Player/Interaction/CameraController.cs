8498da8b61637af37f24e54602ba79a4d6171835
junaidy12 <100924305+junaidy12@users.noreply.github.com>
2023-08-30 13:57:47 +0700

    Camera Raycast Fix

--- Assets/_Project/Scripts/Player/Interaction/CameraController.cs
+++ Assets/_Project/Scripts/Player/Interaction/CameraController.cs
@@ -23,6 +23,7 @@ namespace ThePatient
  
         Camera cam;
         [SerializeField] Interactable interactable;
+        float distance;
         private void OnEnable()
         {
             cam = Camera.main;
@@ -57,7 +58,17 @@ namespace ThePatient
         }
         private void CameraController_TickUpdate(int tick)
         {
-            HandleRaycast();
+            interactable = HandleRaycast();
+            if(interactable != null)
+            {
+                distance = (interactable.transform.position - player.transform.position).sqrMagnitude;
+                if(distance > interactRange * interactRange)
+                {
+                    interactable.OnFinishInteractEvent();
+                    interactable.OnHold = false;
+                    interactable = null;
+                }
+            }
         }
 
         private void Update()
@@ -70,43 +81,40 @@ namespace ThePatient
             transform.position = player.transform.position;
         }
 
-        private void HandleRaycast()
+        private Interactable HandleRaycast()
         {
-            if(Physics.SphereCast(cam.transform.position, interactRayRadius, cam.transform.forward, out RaycastHit hit, interactRayRange, interactLayer))
+            if(Physics.SphereCast(cam.transform.position, interactRayRadius, cam.transform.forward, out RaycastHit hit, interactRayRange))
             {
                 if(hit.transform != null && hit.transform.TryGetComponent<Interactable>(out Interactable interactable))
                 {
-                    float distance = (hit.point - player.transform.position).magnitude;
-                    if(distance <= interactRange)
+                    distance = (hit.point - player.transform.position).sqrMagnitude;
+                    if(distance <= interactRange * interactRange)
                     {
-                        this.interactable = interactable;
                         interactable.OnInteractEvent(interactable.ToString());
-                    }
-                    else
-                    {
-                        interactable.OnFinishInteractEvent();
-                        interactable.OnHold = false;
-                        this.interactable = null;
+                        return interactable;
                     }
                 }
                 else
                 {
-                    if (this.interactable != null)
+                    if(this.interactable != null)
                     {
                         this.interactable.OnFinishInteractEvent();
                         this.interactable.OnHold = false;
+                        return null;
                     }
                 }
             }
             else
             {
-                if (interactable != null)
+                if(interactable != null)
                 {
                     interactable.OnFinishInteractEvent();
                     interactable.OnHold = false;
-                    this.interactable = null;
+                    return null;
                 }
             }
+            
+            return null;
         }
     }
 }
