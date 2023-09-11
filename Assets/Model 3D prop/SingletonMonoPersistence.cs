75f2d93c42bf36717fd215d6db4dadb412455184
Bill Panjaitan <killbill.panjaitan@gmail./com>
2023-09-11 21:52:03 +0700

    Manual Sync
    Manual sync
    
    problem with Merging some file deleted while merging

---
+++ Assets/Model 3D prop/SingletonMonoPersistence.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SingletonMonoPersistence<T> : SingletonMonoBehaviour<SingletonMonoPersistence<T>> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
