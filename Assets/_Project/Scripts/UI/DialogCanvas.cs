062fbfbbb752652e6613cadb9aec14076900599d
junaidy12 <100924305+junaidy12@users.noreply.github.com>
2023-09-21 14:34:13 +0700

    Fix Bug

---
+++ Assets/_Project/Scripts/UI/DialogCanvas.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogCanvas : SingletonBehaviour<DialogCanvas>
+{
+    [field: SerializeField]public TextMeshProUGUI speaker { get; private set; }
+    [field: SerializeField] public TextMeshProUGUI dialogue { get; private set; }
+
+    private void Start()
+    {
+        gameObject.SetActive(false);    
+    }
+}
