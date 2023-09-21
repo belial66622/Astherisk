79e7eb42f47d856b07035c64f95878a184a9f2e7
junaidy12 <100924305+junaidy12@users.noreply.github.com>
2023-09-21 14:59:45 +0700

    Fix Some Bug

--- Assets/_Project/Scripts/UI/DialogCanvas.cs
+++ Assets/_Project/Scripts/UI/DialogCanvas.cs
@@ -6,9 +6,11 @@ public class DialogCanvas : SingletonBehaviour<DialogCanvas>
 {
     [field: SerializeField]public TextMeshProUGUI speaker { get; private set; }
     [field: SerializeField] public TextMeshProUGUI dialogue { get; private set; }
+    [SerializeField] Transform basePanel;
 
     private void Start()
     {
+        basePanel.gameObject.SetActive(true);
         gameObject.SetActive(false);    
     }
 }
